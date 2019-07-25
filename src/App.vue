5866d76b833a4032f2b823efa721eb6dcf267547
erdemnagikho <erdemnayir@gmail.com>
2019-07-26 01:26:16 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form @submit.prevent="searchGifs">
+      <label>Search</label>
+      <input v-model="search" />
+    </form>
+    <div class="loading" v-if="loading">
+      <img src="https://miro.medium.com/max/882/1*9EBHIOzhE1XfMYoKz1JcsQ.gif" alt="loading" />
+    </div>
+    <div class="images" v-if="!loading">
+      <img v-for="gif in gifs" :src="gif" :key="gif" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
+import { value } from "vue-function-api";
+const API_URL =
+  "https://api.giphy.com/v1/gifs/search?api_key=JnxTmEGKXjZeUKBzRjTQoMDg8OX8pS5U&rating=pg&q=";
 export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-  },
+  setup() {
+    const search = value("");
+    const loading = value(false);
+    const gifs = value([]);
+
+    const searchGifs = async () => {
+      loading.value = true;
+      const response = await fetch(API_URL + search.value);
+      const json = await response.json();
+      gifs.value = json.data.map(gif => gif.images.fixed_height.url);
+      setTimeout(() => {
+        loading.value = false;
+      }, 2000);
+    };
+
+    return {
+      search,
+      searchGifs,
+      loading,
+      gifs
+    };
+  }
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.images {
+  column-count: 4;
+}
+
+img {
+  width: 100%;
+}
+
+.loading {
+  margin: 0 auto;
 }
 </style>
